72a727af4426c8c682a9309506c6d322e219f8f0
MrDeys <115146793+MrDeys@users.noreply.github.com>
2026-09-24 14:40:42 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import { MiniMaple } from './miniMaple.js';
+import { MiniMaple } from "./miniMaple.js";
 
-document.addEventListener('DOMContentLoaded',setup)
+document.addEventListener("DOMContentLoaded", setup);
 
 function setup() {
-    const diffButton = document.getElementById('diffButton');
-    diffButton.addEventListener('click', () => {
-        const expressionInput = document.getElementById('expression');
-        const variableInput = document.getElementById('variable');
-        const resultSpan = document.getElementById('result');
-        
-        const miniMaple = new MiniMaple();
-        resultSpan.textContent = miniMaple.diff(expressionInput.value, variableInput.value);
-    });
+  const diffButton = document.getElementById("diffButton");
+  const miniMaple = new MiniMaple();
+  diffButton.addEventListener("click", () => {
+    const expressionInput = document.getElementById("expression");
+    const variableInput = document.getElementById("variable");
+    const resultSpan = document.getElementById("result");
 
+    resultSpan.textContent = miniMaple.diff(
+      expressionInput.value,
+      variableInput.value,
+    );
+  });
 }
